311c488a4ce70c00dd029f6ea79988330f3277e1
Logon Aluno <logonaluno@local.com>
2019-12-14 09:55:13 -0300

    Initial commit

---
+++ app/src/main/java/br/com/onurbasfar/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,11 @@
+package br.com.onurbasfar.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
