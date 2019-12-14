ba8596f767ea5048ff725cf027220076ad75330b
Logon Aluno <logonaluno@local.com>
2019-12-14 10:40:00 -0300

    Before first coffee time.

--- app/src/main/java/br/com/onurbasfar/calculadoraflex/ui/form/FormActivity.kt
+++ app/src/main/java/br/com/onurbasfar/calculadoraflex/ui/form/FormActivity.kt
@@ -2,10 +2,12 @@ package br.com.onurbasfar.calculadoraflex.ui.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.onurbasfar.calculadoraflex.R
 
 class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
     }
 }
--- app/src/main/java/br/com/onurbasfar/calculadoraflex/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/onurbasfar/calculadoraflex/ui/splash/SplashActivity.kt
@@ -1,12 +1,15 @@
 package br.com.onurbasfar.calculadoraflex.ui.splash
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import br.com.onurbasfar.calculadoraflex.R
 import br.com.onurbasfar.calculadoraflex.ui.form.FormActivity
+import br.com.onurbasfar.calculadoraflex.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -15,22 +18,39 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        carregar()
-    }
-    private fun carregar() {
+        val preferences = getSharedPreferences(
+            "user_preferences",
+            Context.MODE_PRIVATE
+        )
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+        if (isFirstOpen) {
+            showLogin()
+        } else {
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        }
+          }
 
-        //Carrega a animacao
+    private fun markAppAlreadyOpen(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
+    }
+
+    private fun showLogin() {
+        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(nextScreen)
+        finish()
+    }
+
+    private fun showSplash() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
         anim.reset()
         ivLogo.clearAnimation()
-
-        //Roda a animacao
         ivLogo.startAnimation(anim)
-
-        //Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
         Handler().postDelayed({
-            val proximaTela = Intent(this@SplashActivity, FormActivity::class.java)
-            startActivity(proximaTela)
+            val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(nextScreen)
             finish()
         }, TEMPO_AGUARDO_SPLASHSCREEN)
     }
